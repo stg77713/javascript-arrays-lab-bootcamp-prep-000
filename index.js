0613b3ffadbccbe9e9a69dfe84bf078bcf36ab01
George Lugo <stg77713@gmail.com>
2020-03-06 03:42:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten(name){
+  kittens.shift(name)
+}
